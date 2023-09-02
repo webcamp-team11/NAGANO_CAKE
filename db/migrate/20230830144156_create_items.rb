2dd8570f8ee746b12e4787f1818ddf036fda05c3
Aya Yamamoto <132640547+achoko3@users.noreply.github.com>
2023-09-02 12:16:06 +0000

    first commit

--- db/migrate/20230830144156_create_items.rb
+++ db/migrate/20230830144156_create_items.rb
@@ -1,7 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.string :name
+      t.string :name, null: false
+      t.string :introduction, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_30_144156) do
-
-  create_table "items", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
